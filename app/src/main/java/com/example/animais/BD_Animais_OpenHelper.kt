dd8abcc411cc1844f59b113c1eba8b7986dc3ed7
Pedro Sousa <sousa_10_99@hotmail.com>
2023-05-02 11:06:56 +0100

    Base de Dados

---
+++ app/src/main/java/com/example/animais/BD_Animais_OpenHelper.kt
@@ -0,0 +1,21 @@
+package com.example.animais
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+private const val NOME_BASE_DADOS = "Animais.db"
+private const val VERSAO_BASE_DADOS = 1
+
+class BD_Animais_OpenHelper (
+    context: Context?,
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
+
+
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    }
+}
