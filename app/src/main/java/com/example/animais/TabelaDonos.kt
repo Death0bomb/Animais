4bdcb5629799ba4934b3555623d1fbc32e5389ba
Pedro Sousa <sousa_10_99@hotmail.com>
2023-05-05 10:44:55 +0100

    Fix na virgula que estava a dar erro trocando por parenteses e por um no final do codigo sql

--- app/src/main/java/com/example/animais/TabelaDonos.kt
+++ app/src/main/java/com/example/animais/TabelaDonos.kt
@@ -5,7 +5,7 @@ import android.provider.BaseColumns
 
 class TabelaDonos(db: SQLiteDatabase) : TabelaBD ( db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA, nome TEXT NOT NULL, numero TEXT, id_categoria INTEGER (NOT NULL), FOREIGN KEY(id_animais) REFERENCES ${TabelaAnimais.NOME_TABElA}(${BaseColumns._ID}) ON DELETE RESTRICT")
+        db.execSQL("CREATE TABLE $NOME_TABELA (nome TEXT NOT NULL, numero TEXT, id_categoria INTEGER NOT NULL, FOREIGN KEY(id_animais) REFERENCES ${TabelaAnimais.NOME_TABElA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
     companion object {
         const val NOME_TABElA = "Animais"
