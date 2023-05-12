f177b87283b1a4aa530793d913832321320eb6f5
Pedro Sousa <sousa_10_99@hotmail.com>
2023-05-12 08:49:52 +0100

    Criar Outra classe

---
+++ app/src/main/java/com/example/animais/Animal.kt
@@ -0,0 +1,4 @@
+package com.example.animais
+
+class Animal(var id: Long -1, var animal:String,var idCategoria:Int) {
+}
---
+++ app/src/main/java/com/example/animais/Categoria.kt
@@ -0,0 +1,5 @@
+package com.example.animais
+
+data class Categoria( var descricao: String,var id: Long = -1) {
+
+}
